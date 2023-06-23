d80312e4409bd2e00300ae234428c32fce4af6ee
wangchangdog <61542937+wangchangdog@users.noreply.github.com>
2023-06-23 16:32:32 +0900

    2023/06/23 授業

---
+++ 3-06_while/while/main.js
@@ -0,0 +1,21 @@
+// 処理。。
+// コンソールに1〜10を連続で表示する(while文とfor文)
+
+// for文の上に
+console.log('for文') // を追加
+for (let i = 1; i < 11; i++) {
+    console.log(i);
+  }
+
+// while文の上に
+console.log('while文')  // を追加
+let i = 1;
+while (i < 11) {
+  console.log(i);
+  i++;
+}
+
+
+
+
+
---
+++ 3-09_fizzbuzz/fizzbuzz/main.js
@@ -0,0 +1,22 @@
+console.log('test')
+/*
+- 引数は num とし number型
+- 3で割り切れる → Fizz!
+- 5で割り切れる → Buzz!
+- 3, 5の両方で割り切れる → FizzBuzz!
+- それ以外は 入力値をそのまま返す
+*/
+
+function fizzbuzz(num) {
+    if ( (num % 3 === 0) && (num % 5 === 0) ) {
+        return "FizzBuzz!";
+    } else if ( num % 3 === 0 ) { // 早期Return
+        return 'Fizz!';
+    } else if (num % 5 === 0 ) {
+        return 'Buzz!'
+    } else {
+        // そのまま返す
+        return num;
+    } 
+}
+fizzbuzz(15); // 何が出てくる?
